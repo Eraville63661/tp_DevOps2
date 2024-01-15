9a6ef17d13bc27280e90ed70c0df99b2661702b6
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-15 17:07:28 +0100

    test7

--- random-picking.py
+++ random-picking.py
@@ -1,27 +1,21 @@
 from math import comb
 
+
 def proba_extraction(k, nb_rouge, nb_blanche, nb_noire):
-
-
     if k < 3:
-        raise ValueError("Le nombre de boules à tirer doit être >=3.")
+        raise ValueError("Le nombre total de boules à tirer doit être au moins 3.")
 
     if nb_rouge + nb_blanche + nb_noire < 3:
-        raise ValueError("Le nombre boules dans l'urne au moins 3.")
+        raise ValueError("Le nombre total de boules dans l'urne doit être au moins 3.")
 
     if nb_rouge < 0 or nb_blanche < 0 or nb_noire < 0:
-        raise ValueError("Le nombre de boules doit être positif.")
+        raise ValueError("Le nombre de boules rouges, blanches et noires doit être positif.")
 
     if k > nb_rouge + nb_blanche + nb_noire:
-        raise ValueError("Pas assez de boules pour tirer k boules.")
+        raise ValueError("Le nombre total de boules dans l'urne est insuffisant pour tirer k boules.")
 
     nombre_combinaisons = comb(nb_rouge, 1) * comb(nb_blanche, 1) * comb(nb_noire, 1)
-
     nombre_total_combinaisons = comb(nb_rouge + nb_blanche + nb_noire, k)
-
     probabilite = nombre_combinaisons / nombre_total_combinaisons
-
-    
     print(f"La probabilité est : {probabilite:.4f}")
     return probabilite
-
