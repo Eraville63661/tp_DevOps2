b4852a424722b0cb7e48c4534bb66e1d92fe10f4
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-15 16:53:36 +0100

    test5

--- random-picking.py
+++ random-picking.py
@@ -2,6 +2,6 @@ def proba(k, nb_rouge, nb_blanche, nb_noire):
     probaN = 0
     probaB = 0
     probaR = 0
-    print(probaN + '..' + probaB + '..' + probaR + '..')
-    print(k + '..' + nb_rouge + '..' + nb_blanche + '..' + nb_noire )
+    print(str(probaN) + '..' + str(probaB) + '..' + str(probaR) + '..')
+    print(str(k) + '..' + str(nb_rouge) + '..' + str(nb_blanche) + '..' + str(nb_noire) )
     return 0
