c1497e05840ee9311a47c26d894e8b1eaa507114
Eraville63661 <45951775+Eraville63661@users.noreply.github.com>
2024-01-15 17:11:48 +0100

    test7

--- random-picking.py
+++ random-picking.py
@@ -3,19 +3,19 @@ from math import comb
 
 def proba_extraction(k, nb_rouge, nb_blanche, nb_noire):
     if k < 3:
-        raise ValueError("Le nombre total de boules à tirer doit être au moins 3.")
+        raise ValueError("Le nombre de boules à tirer doit être au moins 3.")
 
     if nb_rouge + nb_blanche + nb_noire < 3:
-        raise ValueError("Le nombre total de boules dans l'urne doit être au moins 3.")
+        raise ValueError("Le nombre total de boules doit être au moins 3.")
 
     if nb_rouge < 0 or nb_blanche < 0 or nb_noire < 0:
-        raise ValueError("Le nombre de boules rouges, blanches et noires doit être positif.")
+        raise ValueError("Le nombre de boules doit être positif.")
 
     if k > nb_rouge + nb_blanche + nb_noire:
-        raise ValueError("Le nombre total de boules dans l'urne est insuffisant pour tirer k boules.")
+        raise ValueError("Le nombre de boules dans l'urne est insuffisant.")
 
-    nombre_combinaisons = comb(nb_rouge, 1) * comb(nb_blanche, 1) * comb(nb_noire, 1)
-    nombre_total_combinaisons = comb(nb_rouge + nb_blanche + nb_noire, k)
-    probabilite = nombre_combinaisons / nombre_total_combinaisons
+    nb_comb = comb(nb_rouge, 1) * comb(nb_blanche, 1) * comb(nb_noire, 1)
+    nb_t_combi = comb(nb_rouge + nb_blanche + nb_noire, k)
+    probabilite = nb_comb / nb_t_combi
     print(f"La probabilité est : {probabilite:.4f}")
     return probabilite
